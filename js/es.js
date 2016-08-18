39b035af9cca6f8449e9b446a5b0281f74e5dfc7
Willy McAllister <willy@khanacademy.org>
2016-08-18 09:43:17 -0700

    professionally translated es.js

--- js/es.js
+++ js/es.js
@@ -1,21 +1,21 @@
 var i18n = {
-error1: 'Lo sentimos, hay un error del navegador en empezando la herramienta esquemática. Recomendamos el uso de las versiones más nuevas de Firefox y Chrome.',
+error1: 'Lo sentimos, hay un error del navegador al iniciar la herramienta esquemática. Recomendamos el uso de las versiones más nuevas de Firefox y Chrome.',
 Ground_connection: 'Conexión a tierra',
-Node_label: 'Etiqueta de un nodo',
+Node_label: 'Etiqueta para nodo',
 Voltage_source: 'Fuente de voltaje',
 Current_source: 'Fuente de corriente',
 Resistor: 'Resistor',
 Capacitor: 'Condensador',
 Inductor: 'Inductor',
-Op_Amp: 'Op Amp',
+Op_Amp: 'Amp Op',
 Diode: 'Diodo',
 NFet: 'NFet',
 PFet: 'PFet',
-Voltage_probe: 'sonda de voltaje',
-Current_probe: 'sonda de corriente',
-drag_onto_diagram: ': arrastre o toque para insertar',
+Voltage_probe: 'Sensor de voltaje',
+Current_probe: 'Sensor de corriente',
+drag_onto_diagram: ': arrastre o pulse para insertar',
 Help: 'Ayuda: página de ayuda',
-Grid: 'Grid: toggle grid display',
+Grid: 'Rejilla: Mostrar/ocultar rejilla',
 Cut: 'Cortar componentes seleccionados al portapapeles',
 Copy:'Copiar componentes seleccionados al portapapeles',
 Paste:'Pegar portapapeles al esquema',
@@ -24,64 +24,63 @@ Rotate:'Rotar componente seleccionado',
 Save_netlist: 'Guardar la lista de conexiones',
 Open_netlist: 'Abrir la lista de conexiones',
 Select_netlist: 'Seleccionar netlist',
-Perform_DC_Analysis: 'Realizar un Análisis DC',
+Perform_DC_Analysis: 'Realizar análisis DC',
 DC_Analysis: 'Análisis DC',
-AC_Small_Signal_Analysis: 'Realizar un Análisis de AC Pequeño-Señal',
-Perform_Transient_Analysis: 'Realizar un análisis de Transitorios',
-Transient_Analysis: 'Análisis de Transitorios',
-Edit_Properties: 'Editar Propiedades',
+AC_Small_Signal_Analysis: 'Realizar análisis AC de pequeña-señal',
+Perform_Transient_Analysis: 'Realizar Análisis transitorio',
+Transient_Analysis: 'Análisis transitorio',
+Edit_Properties: 'Editar propiedades',
 
 points_per_decade: 'Número de puntos/década',
-Starting_frequency: 'Frecuencia de arranque (Hz)',
-Ending_frequency: 'Frecuencia de fin (Hz)',	
+Starting_frequency: 'Frecuencia inicial (Hz)',
+Ending_frequency: 'Frecuencia final (Hz)',	
 source_for_ac: 'Nombre de fuente V o I  para ac',
-AC_Analysis_add_a_voltage_probe: "Análisis de AC: añadir una sonda de voltaje al diagrama!",
-AC_Analysis: 'Análisis de AC',
-Zero_ac_response: 'Cero respuesta de ac, -infinity en la escala de Db.',
-Near_zero_ac_response: 'Cerca de respuesta de ac a cero, sacar ',
-probe: ' sonda',
+AC_Analysis_add_a_voltage_probe: "Análisis AC: ¡añadir un sensor de voltaje al diagrama!",
+AC_Analysis: 'Análisis AC',
+Zero_ac_response: 'Respuesta cero de ac, -infinito en la escala DB.',
+Near_zero_ac_response: 'Cerca de respuesta cero de ac, expulsar ',
+sensor: ' sensor',
 
 // Alerts and warnings from the circuit simulator
 Alert: '¡Alerta!',
-ckt_alert1: '¡¡¡Advertencia!!! Circuito tiene un circuito de fuente de tensión o una fuente o sonda de corriente en cortocircuito por un cable, por favor, elimine la fuente o el cable que causa el corto.',
-ckt_alert2: '¡¡¡Advertencia!!! Simulador podría producir resultados sin sentido o sin resultado con circuitos ilegales.',
-ckt_warning1: 'Advertencia: dos elementos de circuito comparten el mismo nombre ',
+ckt_alert1: '¡¡¡Advertencia!!! El circuito tiene bucle en la fuente de voltaje o o una fuente o sensor de corriente haciendo cortocircuito en un cable, favor de quitar la fuente o el cable que causa el corto.',
+ckt_alert2: '¡¡¡Advertencia!!! El simulador podría producir resultados sin sentido o sin resultados con circuitos ilegales.',
+ckt_warning1: 'Advertencia: dos elementos del circuito comparten el mismo nombre ',
 ckt_alert3: 'Por favor, haga al menos una conexión a tierra (símbolo del triángulo)',
-ckt_alert4: 'Método de Newton falló. ¿Sus fuentes de corriente tienen un camino conductor a tierra?',
-ckt_alert5: 'Método de Newton no, puede ser su circuito o puede ser nuestro simulador.',
+ckt_alert4: 'El método de Newton falló. ¿Sus fuentes de corriente tienen una conexión a tierra?',
+ckt_alert5: 'El método de Newton falló, puede ser su circuito o puede ser nuestro simulador.',
 ckt_alert6: 'DC falló, tratando análisis transitorio de cero.',
-ckt_alert7: 'Análisis AC se refiere a fuente desconocida ',
+ckt_alert7: 'Análisis AC refiere a una fuente desconocida ',
 ckt_alert8: 'Análisis AC falló, fuente desconocida',
 
-ckt_error1: 'Las filas de M cargado no coincide con b cols desajuste a x.',
-ckt_error2: 'Fila o columnas de A es demasiado grande para B',
-ckt_error3: 'Fila o columnas de A es demasiado grande para C',
-ckt_error4: 'scalea y scaleb deben ser escalares o Matrices',
-ckt_error5: 'Las filas ni columnas > filas ni columnas de destino',
-ckt_error6: 'Las filas ni columnas > columnas ni filas de destino',	 
+ckt_error1: 'Los renglones M no coinciden con b o hay un desajuste con las columnas x.',
+ckt_error2: 'Renglones o columnas de A demasiado grandes para B',
+ckt_error3: 'Renglones o columnas de A demasiado grande para C',
+ckt_error4: 'scalea y scaleb deben ser escalares o Arreglos',
+ckt_error5: 'Renglones o columnas > renglones o columnas de destino',
+ckt_error6: 'Renglones o columnas > columnas o renglones de destino',	 
 
-// added since sharing with Jake
 log_Frequency: 'log (Frecuencia en Hz)',
 degrees: 'grados',
-AC_Phase: 'Fase de CA',
+AC_Phase: 'Fase AC',
 AC_Magnitude: 'Magnitud de AC',
 
 Minimum_number_of_timepoints: 'Número mínimo de puntos de tiempo',
-Stop_time_seconds: 'Detener el tiempo (segundos)',
-Transient_Analysis_add_a_probe: 'Análisis de transitorios: añadir un sondeo al diagrama!',
+Stop_time_seconds: 'Detener en el tiempo (segundos)',
+Transient_Analysis_add_a_probe: 'Análisis transitorio: ¡añadir un sensor al diagrama!',
 
 //Use creating phrasing to get this right: 
-// alert('The ' + color + ' probe is connected to node ' + '"' + label + '"' + ' which is not an actual circuit node');
-The: 'El sonda ',
+// alert('El sensor ' + color + ' está conectado al nodo ' + '"' + label + '"' + ' que no es un nodo del circuito en este momento');
+The: 'El sensor ',
 probe_is_connected_to_node: ' está conectado al nodo ',
-which_is_not_an_actual_circuit_node: ' que no es un nodo de circuito real',
+which_is_not_an_actual_circuit_node: ' que no es un nodo del circuito en este momento',
 
 Voltage: 'Voltaje',
 Current: 'Corriente',
 Time: 'Tiempo',
 Node_has_two_conflicting_labels: 'Nodo tiene dos etiquetas en conflicto: ',
 
-DC_value: 'Valor de DC',
+DC_value: 'Valor DC',
 
 impulse: 'impulso',
 Height: 'Altura',
@@ -89,8 +88,8 @@ Width: 'Ancho (segundos)',
 
 step: 'paso',
 Initial_value: 'Valor inicial',
-Plateau_value: 'Valor meseta',					//??
-Delay_until_step: 'Retardo hasta que el paso (seg)',
+Plateau_value: 'Valor meseta',
+Delay_until_step: 'Retardo hasta el paso (seg)',
 Rise_time: 'Tiempo de subida (seg)',
 
 square: 'cuadrado',
@@ -105,33 +104,33 @@ Comma_separated_list: 'Lista separada por comas de los tiempos y valores alterno
 pwl_repeating: 'pwl_repeating',
 
 pulse: 'pulso',
-Delay_until_pulse: 'Retardo hasta que el pulso (seg)',
+Delay_until_pulse: 'Retardo hasta el pulso (seg)',
 Time_for_first_transition: 'Tiempo para la primera transición (seg)',
 Time_for_second_transition: 'Tiempo para la segunda transición (seg)',
 Pulse_width: 'Ancho de pulso (segundos)',
 Period: 'Período (seg)',
 
-sin: 'sin',
+sin: 'seno',
 Offset_value: 'Valor de desplazamiento',
 Amplitude: 'Amplitud',
-Delay_until_sin_starts: 'Retardo hasta que se inicia el sino (seg)',	//??
+Delay_until_sin_starts: 'Retardo hasta el inicio del seno (seg)',
 Phase_offset_degrees: 'Desplazamiento de fase (grados)',
 
-Circuit_Sandbox_Help: 'CIRCUIT SANDBOX HELP',
+Circuit_Sandbox_Help: 'AYUDA PARA PRUEBAS',
 name: 'nombre',
 value: 'valor',
-label: 'label',		//??
+label: 'etiqueta',
 r: 'r',
 c: 'c',
 l: 'l',
 color: 'color',
 offset: 'compensar',
 area: 'área',
-type: 'typo',
+type: 'tipo',
 WL: 'W/L',
 A: 'A',
-Plot_color: 'Color de graphico',
-Plot_offset: 'Compensar de graphico',
+Plot_color: 'Color del gráfico',
+Plot_offset: 'Compensar gráfica',
 dc: 'dc',
 impulse: 'impulso',
 step: 'paso',
@@ -142,21 +141,21 @@ pwl_repeating: 'pwl de repetición',
 pulse: 'pulso',
 sin: 'seno',
 
-last_line: 'last, no comma'
+last_line: 'última líena sin coma'
 };
 
 // translated color names, not to be confused with variables: probe_names or probe_colors or probe_colors_rgb
-var i18n_probe_cnames = ["rojo", "verde", "azul", "cian", "magenta", "amarillo", "negro",'x-axis'];
+var i18n_probe_cnames = ["rojo", "verde", "azul", "cian", "magenta", "amarillo", "negro",'eje-x'];
 
-var strSHelp = "CIRCUITO SALVADERA AYUDA \n\n"; // Ayuda incrustado
-var strAddC = "Añadir componente: Haga clic en una parte en la papelera, a continuación, haga clic en el esquema para añadir. \n\n";
-var strAddW = "Añadir alambre: Los cables comienzan a partir de los puntos de conexión (círculos abiertos) Haga clic en una conexión para iniciar un hilo, arrastre y suelte. \n\n";
-var strSel = "Seleccionar: Arrastre un rectángulo para seleccionar los componentes. Shift clic para incluir otro componente. \n\n";
-var strMove = "Mover: Haga clic para seleccionar, a continuación, arrastre a una nueva ubicación. \n\n";
-var strDel = "Borrar: Seleccionar, a continuación, haga clic en el icono X o golpear RETROCESO. \n\n";
-var strRot = "Rotar/Reflejar: Haga clic para seleccionar, a continuación, haga clic en el icono de rotación o escriba la letra \"r\" para girar 90. Repetir para más rotaciones y reflexiones. \n\n";
+var strSHelp = "AYUDA DEL CIRCUITO DE PRUEBAS \n\n"; // Ayuda incrustado
+var strAddC = "Añadir componente: Haga clic en una parte en la papelera y a continuación haga clic en el esquema para añadir. \n\n";
+var strAddW = "Añadir cable: los cables comienzan a partir de los puntos de conexión (círculos abiertos). Haga clic en una conexión para iniciar un cable, arrastre y suelte. \n\n";
+var strSel = "Seleccionar: Arrastre en rectángulo para seleccionar los componentes. Shift clic para incluir otro componente. \n\n";
+var strMove = "Mover: Haga clic para seleccionar y a continuación arrastre a una nueva ubicación. \n\n";
+var strDel = "Borrar: Seleccionar y a continuación haga clic en el icono X o presione RETROCESO. \n\n";
+var strRot = "Rotar/Reflejar: Haga clic para seleccionar, a continuación, haga clic en el icono de rotación o presione la letra \"r\" para girar 90. Repetir para más rotaciones y reflexiones. \n\n";
 var strProp = "Propiedades: Haga doble clic en un componente para cambiar los valores. \n\n";
-var strNum = "Sufijos numéricos se puede introducir en la notación de ingeniería: \n\
+var strNum = "Sufijos numéricos se pueden introducir con notación de ingeniería: \n\
 T\t10^12\t\tm\t10^-3 \n\
 G\t10^9 \t\tu\t10^-6  \n\
 M\t10^6 \t\tn\t10^-9  \n\
